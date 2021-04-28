a19f3f98410edf91c23ed4ba5a18d68ba1f6a341
Karamelka322 <aafanya@bk.ru>
2021-04-28 15:00:33 +0700

    First commit

---
+++ Assets/3. GameManager/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Animation _mainMenuAnimator;
+    [SerializeField] private AnimationClip _fadeOutAnimation;
+    [SerializeField] private AnimationClip _fadeInAnimation;
+
+    [SerializeField] private Button _levelOne;
+    [SerializeField] private Button _levelTwo;
+
+    public Events.EventFadeComplete OnFadeComplete;
+
+    private void Start()
+    {
+        GameManager.Instance.OnGameStateChanged.AddListener(HandleGameStateChanged);
+
+        _levelOne.onClick.AddListener(LoadLevel1);
+        _levelTwo.onClick.AddListener(LoadLevel2);
+    }
+
+    private void LoadLevel1()
+    {
+        GameManager.Instance.LoadLevel("Level 1");
+    }
+
+    private void LoadLevel2()
+    {
+        GameManager.Instance.LoadLevel("Level 2");
+    }
+
+    public void OnFadeOutComplete()
+    {
+        OnFadeComplete.Invoke(true);
+    }
+
+    public void OnFadeInComplete()
+    {
+        OnFadeComplete.Invoke(false);
+        UIManager.Instance.SetDummyCameraActive(true);
+    }
+
+    void HandleGameStateChanged(GameManager.GameState currentState, GameManager.GameState previousState)
+    {
+        if (_mainMenuAnimator == null)
+        {
+            return;
+        }
+
+        if (previousState == GameManager.GameState.PREGAME && currentState != GameManager.GameState.PREGAME)
+        {
+            UIManager.Instance.SetDummyCameraActive(false);
+            _mainMenuAnimator.clip = _fadeOutAnimation;
+            _mainMenuAnimator.Play();
+        }
+
+        if (previousState != GameManager.GameState.PREGAME && currentState == GameManager.GameState.PREGAME)
+        {
+            _mainMenuAnimator.Stop();
+            _mainMenuAnimator.clip = _fadeInAnimation;
+            _mainMenuAnimator.Play();
+        }
+    }
+}
